caac4b211e5ee4c2ef41b64fdf33eb42fe5a8062
octkp <takano_y@kokopelli-inc.com>
2024-02-13 09:12:51 +0900

    Projects to dev

--- zsh/kokopelli_alias.zsh
+++ zsh/kokopelli_alias.zsh
@@ -8,56 +8,56 @@ alias ksail="bash sail"
 alias awslogin="aws sso login --profile ba_developers"
 alias jump-login-test="ssh -l ec2-user i-066b16d5ec5ec8f8c"
 
-# projects
-alias ba="cd ~/Projects/ba"
-alias badev="cd ~/Projects/ba/badev"
-alias bigadvance="cd ~/Projects/ba/bigadvance"
+# dev
+alias ba="cd ~/dev/ba"
+alias badev="cd ~/dev/ba/badev"
+alias bigadvance="cd ~/dev/ba/bigadvance"
 
 #postgres
 alias pqlogin="dc exec db psql -U webapp -d"
 
 # tests
-alias batest="~/Projects/ba/bigadvance/src/xba/phpunit/run.sh --testdox"
-alias batest-up="~/Projects/ba/bigadvance/src/xba/phpunit/up.sh"
-alias batest-down="~/Projects/ba/bigadvance/src/xba/phpunit/down.sh"
-alias batest-coverage="~/Projects/ba/bigadvance/src/xba/phpunit/run.sh --testdox --coverage-html coverage-result"
-alias batest-cd="cd ~/Projects/ba/bigadvance/src/xba/phpunit"
+alias batest="~/dev/ba/bigadvance/src/xba/phpunit/run.sh --testdox"
+alias batest-up="~/dev/ba/bigadvance/src/xba/phpunit/up.sh"
+alias batest-down="~/dev/ba/bigadvance/src/xba/phpunit/down.sh"
+alias batest-coverage="~/dev/ba/bigadvance/src/xba/phpunit/run.sh --testdox --coverage-html coverage-result"
+alias batest-cd="cd ~/dev/ba/bigadvance/src/xba/phpunit"
 alias apitest="docker compose exec phpunit vendor/bin/phpunit --testdox"
 alias apitest-coverage="docker compose exec phpunit vendor/bin/phpunit --testdox --coverage-html coverage-result"
 
 #migration
 migration-xba() {
-  cd ~/Projects/ba/bigadvance/src/xba
+  cd ~/dev/ba/bigadvance/src/xba
   docker/migration.sh "$1"
 }
 
 migration-bam() {
-  cd ~/Projects/ba/bigadvance/src/bamanager
+  cd ~/dev/ba/bigadvance/src/bamanager
   docker/migration.sh  bamanager
 }
 
 # batch
 batch-xba() {
-  cd ~/Projects/ba/badev
+  cd ~/dev/ba/badev
   docker compose run --rm -u webapp -e "XBA_SYSTEM_CODE=$1" xba ./mk refine "$2"
 }
 
 batch-bam() {
-  cd ~/Projects/ba/badev
+  cd ~/dev/ba/badev
   docker compose run --rm -u webapp bamanager ./mk refine "$1"
 }
 
 badev-up() {
-  cd ~/Projects/ba/badev
+  cd ~/dev/ba/badev
   docker compose up -d bamanager bamanager-bg big-advance-api ba-admin-frontend ba-admin-backend ba-idp xba-front ba-site-frontend chat-api chat-api-bg ba-chat minio
 }
 
 badev-down() {
-  cd ~/Projects/ba/badev
+  cd ~/dev/ba/badev
   docker compose down
 }
 
 badev-restart() {
-  cd ~/Projects/ba/badev
+  cd ~/dev/ba/badev
   docker compose restart bamanager bamanager-bg big-advance-api ba-admin-frontend ba-admin-backend ba-idp xba-front ba-site-frontend chat-api chat-api-bg ba-chat minio
 }
